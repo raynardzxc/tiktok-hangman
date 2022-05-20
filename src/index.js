5d6307e50adf7cc4cecbc4103f7986a23fa0520f
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2022-05-20 16:43:23 +0800

    Add dark mode

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import storage from 'local-storage-fallback';
+import {
+  ThemeProvider,
+  createGlobalStyle
+} from 'styled-components';
+import useTheme from './useTheme';
+import ToggleMode from './Components /ToggleMode.js';
+import style from 'styled-theming';
+import './style.css';
+import Hangman from './Hangman/Hangman';
+
+const getBackground = style('mode', {
+  light: '#EEE',
+  dark: '#111'
+});
+
+const getForeground = style('mode', {
+  light: '#111',
+  dark: '#EEE'
+});
+
+
+const GlobalStyle = createGlobalStyle`
+body {
+  background-color: ${getBackground};
+  color: ${getForeground};
+}
+`;
+
+function App() {
+  const theme = useTheme();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyle />
+        <div className="App">
+          <Hangman/>
+          <ToggleMode />
+        </div>
+      </>
+    </ThemeProvider>
+  );
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
